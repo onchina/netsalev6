d99f45e8abd9ca38280867f4a7a6ea86885e5ada
onchina <itworld.com@gmail.com>
2026-02-25 23:21:02 +0800

    清理

--- backend/test_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-login_data = {
-    "username": "admin",
-    "password": "password123" # assuming default maybe? Or check tests
-}
-# Try to login first, if we can't we'll just generate a JWT
